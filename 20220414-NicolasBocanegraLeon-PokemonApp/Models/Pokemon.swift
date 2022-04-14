c8807b5addcc7619b4927b45e32c15941883190d
Nicolás Bocanegra León <nicolasbocanegra@icloud.com>
2022-04-14 16:55:00 -0400

    P-001: Adding model structs.

--- 20220414-NicolasBocanegraLeon-PokemonApp/Models/Pokemon.swift
+++ 20220414-NicolasBocanegraLeon-PokemonApp/Models/Pokemon.swift
@@ -5,19 +5,25 @@
 //  Created by Nicolás Bocanegra León on 4/14/22.
 //
 
-import UIKit
+import Foundation
 
-class Pokemon: Codable {
+struct Pokemon: Codable {
     var id: Int
     var name: String
-    var url:
-    image
-    stats
-    var abilities: [PokemonAbility]
+    var imageUrl: String?
+    var stats: [PokemonStat]?
+    var abilities: [PokemonAbility]?
 }
-struct PokemonAbility {
+
+struct PokemonAbility: Codable {
     var name: String
     var url: String
     var isHidden: Bool
     var slot: Int
+}
+
+struct PokemonStat: Codable {
+    var stat: String
+    var effort: Int
+    var base_stat: Int
 }
